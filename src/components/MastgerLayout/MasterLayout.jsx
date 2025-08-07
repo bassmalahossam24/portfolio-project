d593c3c925c93297dd41d68b04565ad99c549845
bassmalahossam24 <125506834+bassmalahossam24@users.noreply.github.com>
2025-08-07 21:50:37 +0300

    fix the layout

--- src/components/MastgerLayout/MasterLayout.jsx
+++ src/components/MastgerLayout/MasterLayout.jsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import Sidebar from '../Sidebar/Sidebar'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Sidebar from '../Sidebar/Sidebar';
+import { Outlet } from 'react-router-dom';
+import './MasterLayout.css'
 
 export default function MasterLayout() {
   return (
-    <div>
-      <div className="row  gx-0">
+    <div className="container-fluid">
+      <div className="row gx-0">
         <div className="col-md-3">
-            <Sidebar/>
-            </div>
-            <div className="col-md-9">
-             <Outlet/>
-            
-              </div>
-            
+          <div className="sidebar-wrapper">
+            <Sidebar />
+          </div>
+        </div>
+        <div className="col-md-9 m-0">
+          <Outlet />
+        </div>
       </div>
-        
-     
     </div>
-  )
+  );
 }
